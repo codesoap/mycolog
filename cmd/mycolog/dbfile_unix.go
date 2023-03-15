f5b4172ff987ebe00e1fd75ae810df2d337b1f7a
codesoap <codesoap@mailbox.org>
2023-03-15 11:09:23 +0100

    Create directory for database file if necessary

--- cmd/mycolog/dbfile_unix.go
+++ cmd/mycolog/dbfile_unix.go
@@ -5,6 +5,7 @@ package main
 
 import (
 	"errors"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -17,6 +18,12 @@ func getDBFilename() (string, error) {
 			return "", errors.New("could not find a place for the database")
 		}
 		dbDir = filepath.Join(home, ".local", "share")
+	}
+	if _, err := os.Stat(dbDir); os.IsNotExist(err) {
+		log.Printf("Creating directory '%s' for the database file.\n", dbDir)
+		if err := os.MkdirAll(dbDir, 0755); err != nil {
+			return "", err
+		}
 	}
 	return filepath.Join(dbDir, "mycolog.sqlite3"), nil
 }
